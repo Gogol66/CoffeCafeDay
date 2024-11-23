7db98f522f99e66f13f896bbb26b612e47c1ac5f
Gogol66 <sauravbhattacharjee967@gmail.com>
2024-11-24 01:04:47 +0530

    Created Project CoffeCafeDay

---
+++ OrderDetails.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeCafeDay.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+      public int OrderDetailID { get; set; }
+        [Required]
+        [ForeignKey("OrderID")]
+    public Orders? Orders { get; set; }
+        [Required]
+        [ForeignKey("ProductID")]
+        public Products? Products { get; set; }
+      public int Quantity { get; set; }
+      public double Price { get; set; }
+    }
+}
