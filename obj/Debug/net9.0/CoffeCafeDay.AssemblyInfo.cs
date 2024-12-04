c797e04d9a9ef3a43fa86b65d2cf236e760190aa
Gogol66 <sauravbhattacharjee967@gmail.com>
2024-12-04 23:23:59 +0530

    Changes in index.html

--- obj/Debug/net9.0/CoffeCafeDay.AssemblyInfo.cs
+++ obj/Debug/net9.0/CoffeCafeDay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeCafeDay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478d5c898eeafe290530c3ee74a065113443b3bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c59f053b2b2f9e5642f558fe2fef7cf98d7ccb14")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeCafeDay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeCafeDay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
